06e2d77b740c24d3a439748cc37179d0dd6641bb
foday <foday_s@yahoo.com>
2020-12-24 13:21:39 -0600

    Group B-Step 3: shell
    Do a quick query by of the Question class by typing 'Question.objects.all()' which should output the question that was saved.
    Now save Question.objects.get(pk=1) into q since Quetion is related to Choice so type 'q = Question.objects.get(pk=1)'
    check to see that is in the choice_set by typing q.choice_set.all() which will produce an empty array.
    So we create some choices by using the create method, type 'q.choice_set.create(choice_text='Django', votes=0)' then i did the same for the other 3 choices.
    quit by typing quit()

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                ('Date Information', {'fields': ['publish_date'], 'classes':
+                ['collapse']}),]
+    inLines = [ChoiceInLine]
+
+admin.site.register(Question, QuestionAdmin)
